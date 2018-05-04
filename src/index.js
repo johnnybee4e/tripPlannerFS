c6475eec3c0d85143c27be9d8004ed2c6a6daae5
johnnybee4e <jon.rosado42@gmail.com>
2018-05-04 12:27:09 -0400

    trip planner

---
+++ src/index.js
@@ -0,0 +1,11 @@
+console.log('HELLO FROM JAVASCRIPT!');
+const mapboxgl = require('mapbox-gl');
+console.log(mapboxgl);
+mapboxgl.accessToken = 'pk.eyJ1IjoiZnVsbHN0YWNram9uIiwiYSI6ImNqZ3M1OTcwcjAwMHMzNGxubXlxbHFxaHoifQ.LYHfQzOU5Hb3GF2JkOJYZQ';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705],
+  zoom: 12,
+  style: "mapbox://styles/mapbox/streets-v10"
+});
